a3aa7e11763c353f334177e0523090bb4a685ab3
Iago Cavalcante <iagoangelimc@gmail.com>
2023-02-28 21:01:20 -0300

    chore: change link to resume

--- lib/iagocavalcante_web/live/home.ex
+++ lib/iagocavalcante_web/live/home.ex
@@ -94,7 +94,7 @@ defmodule IagocavalcanteWeb.HomeLive do
                   </ol>
                   <a
                     class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-                    href="./doc/iago_resume.pdf"
+                    href="https://iagocavalcante.s3.amazonaws.com/iago_resume.pdf"
                     download
                   >
                     <%= gettext("Download CV") %><svg
