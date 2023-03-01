bec02bcc59a4fea0e1ab0bd6a553a47e4e43f0df
Iago Cavalcante <iagoangelimc@gmail.com>
2023-02-28 21:35:40 -0300

    fix: PHX HOST and target blank in CV

--- lib/iagocavalcante_web/live/home.ex
+++ lib/iagocavalcante_web/live/home.ex
@@ -96,6 +96,7 @@ defmodule IagocavalcanteWeb.HomeLive do
                     class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
                     href="https://iagocavalcante.s3.amazonaws.com/iago_resume.pdf"
                     download
+                    target="_blank"
                   >
                     <%= gettext("Download CV") %><svg
                       viewBox="0 0 16 16"
