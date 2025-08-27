7146f9da7ad3c6c8254459d908b11382b0d96dda
Iago Cavalcante <iagocavalcante@hey.com>
2025-08-27 18:50:16 -0300

    config ssl options

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :iagocavalcante, Iagocavalcante.Repo,
-    ssl: true,
+    ssl: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
