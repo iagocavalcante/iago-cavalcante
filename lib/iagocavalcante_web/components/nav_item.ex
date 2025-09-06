afbb3fa234011fcfd5881cffc22230f12e65b9ad
Iago Cavalcante <iagocavalcante@hey.com>
2025-09-06 11:08:49 -0300

    fixing menu

--- lib/iagocavalcante_web/components/nav_item.ex
+++ lib/iagocavalcante_web/components/nav_item.ex
@@ -11,7 +11,7 @@ defmodule IagocavalcanteWeb.NavItem do
     <li>
       <a class={["relative block transition", desktop_and_mobile_classes(assigns)]} href={@link} @rest>
         <%= @text %>
-        <%= if atom_to_string(@active_item) =~ String.downcase(@text) do %>
+        <%= if String.downcase(@text) == atom_to_string(@active_item) do %>
           <span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0 hidden md:block">
           </span>
         <% end %>
@@ -23,11 +23,11 @@ defmodule IagocavalcanteWeb.NavItem do
   defp desktop_and_mobile_classes(assigns) do
     text_downcase = String.downcase(assigns.text)
     active_item = atom_to_string(assigns.active_item)
-    
+
     base_classes = "px-3 py-2 md:px-3 md:py-2"
     mobile_specific = "md:rounded-none py-3 text-base"
-    
-    if active_item =~ text_downcase do
+
+    if text_downcase == active_item do
       [base_classes, mobile_specific, "text-teal-500 dark:text-teal-400"]
     else
       [base_classes, mobile_specific, "hover:text-teal-500 dark:hover:text-teal-400"]
